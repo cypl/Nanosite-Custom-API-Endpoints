f87e122afb28608a2c06a0fdb680276c70e1c072
cypl <cyrille.plenchette@me.com>
2023-06-19 22:29:07 +0200

    fix url menus

--- endpoints/menus.php
+++ endpoints/menus.php
@@ -10,28 +10,28 @@ Retrieves data from registered menus (only work for public menu):
       "items": [
         {
           "title": "Qui sommes-nous",
-          "url": "http://localhost:8888/headless-wp/qui-sommes-nous/",
+          "url": "/qui-sommes-nous/",
           "children": [
             {
               "title": "Ma liste de bateaux",
-              "url": "http://localhost:8888/headless-wp/ma-liste-de-bateaux/",
+              "url": "/ma-liste-de-bateaux/",
               "children": []
             },
             {
               "title": "Déclaration d’accessibilité",
-              "url": "http://localhost:8888/headless-wp/declaration-daccessibilite/",
+              "url": "/declaration-daccessibilite/",
               "children": []
             }
           ]
         },
         {
           "title": "Contact",
-          "url": "http://localhost:8888/headless-wp/contact/",
+          "url": "/contact/",
           "children": []
         },
         {
           "title": "Plan du site",
-          "url": "http://localhost:8888/headless-wp/plan-du-site/",
+          "url": "/plan-du-site/",
           "children": []
         }
       ]
@@ -43,6 +43,8 @@ Retrieves data from registered menus (only work for public menu):
 function get_menus_data() {
     $menus = get_registered_nav_menus();
     $data = array();
+
+    $home_url = home_url(); // Obtenir l'URL de la page d'accueil
 
     foreach ( $menus as $location => $description ) {
         $items = wp_get_nav_menu_items( $location );
@@ -59,11 +61,13 @@ function get_menus_data() {
             foreach ( $all_menu_items as $item ) {
                 // Vérifier la visibilité de l'élément pour les utilisateurs non connectés
                 if ( $item->menu_item_parent == 0 && ( ! is_user_logged_in() || current_user_can( 'read' ) ) ) {
+                    $route = str_replace( $home_url, '', $item->url ); // Supprimer l'URL de la page d'accueil de l'URL complète
+
                     $children = get_menu_item_children( $item->ID, $all_menu_items );
 
                     $menu_items[] = array(
                         'title'    => $item->title,
-                        'url'      => $item->url,
+                        'url'      => $route,
                         'children' => $children,
                     );
                 }
@@ -79,19 +83,19 @@ function get_menus_data() {
     return $data;
 }
 
-
-
 // Fonction récursive pour récupérer les éléments enfants d'un item de menu
 function get_menu_item_children( $parent_id, $all_menu_items ) {
     $menu_items = array();
 
     foreach ( $all_menu_items as $item ) {
         if ( $item->menu_item_parent == $parent_id ) {
+            $route = str_replace( $home_url, '', $item->url ); // Supprimer l'URL de la page d'accueil de l'URL complète
+
             $children = get_menu_item_children( $item->ID, $all_menu_items );
 
             $menu_items[] = array(
                 'title'    => $item->title,
-                'url'      => $item->url,
+                'url'      => $route,
                 'children' => $children,
             );
         }
@@ -99,4 +103,3 @@ function get_menu_item_children( $parent_id, $all_menu_items ) {
 
     return $menu_items;
 }
-
